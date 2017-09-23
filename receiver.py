986872bd3d751dd1b8b0ed4aa25ac6317d56094a
Rupsha <rupsha.bagchi@gmail.com>
2017-09-23 17:32:40 +0300

    forward error correction

--- receiver.py
+++ receiver.py
@@ -9,6 +9,8 @@ def main():
     host="localhost"
     port=12345
     received_packet = 0
+    message_sequence = '0'
+    actual_message = ""
     try:
         s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
         s.bind((host,port))
@@ -22,13 +24,27 @@ def main():
             # Set timeout of one minute for socket operations
             # s.settimeout(60)
             packet,address = s.recvfrom(1024)
-            message_decoded = packet.split('#')
-            time_sent = datetime.datetime.strptime(message_decoded[1],"%Y-%m-%d %H:%M:%S.%f")
-            print "packet {}, sent at Time: {}".format(message_decoded[0],time_sent)
-            received_packet+=1
-            #Time delay in receiving packet transmission
-            delay = datetime.datetime.now()-time_sent
-            print "reception delay in seconds= {}".format(delay.total_seconds())
+            received_packet += 1
+            actual_message_prev = actual_message
+            message_sequence_prev = message_sequence
+            message_received = packet.split('#')
+            message_sequence = packet.split('->')[0]
+            actual_message = message_received[0].split('->')[1]
+            print "actual message prev"+actual_message_prev
+            print "\n actual message"+actual_message
+            print "\n\n\n"
+            time_sent = datetime.datetime.strptime(message_received[1],"%Y-%m-%d %H:%M:%S.%f")
+            #detect redundant packets
+            if (message_sequence == message_sequence_prev):
+                continue
+            else:
+                if (message_sequence == ""):
+                    msg = fec_xor(actual_message_prev,actual_message)
+                    print msg
+                # print "packet {}, sent at Time: {}".format(message_received[0],time_sent)
+                # #Time delay in receiving packet transmission
+                # delay = datetime.datetime.now()-time_sent
+                # print "reception delay in seconds= {}".format(delay.total_seconds())
 
         #safe exit
         except KeyboardInterrupt:
--- sender.py
+++ sender.py
@@ -19,7 +19,6 @@ def main():
     host = "localhost"
     port = 12345
     initial_packet = [] # initial packet
-    injected_packet = [] #list after dropping some packets
     xor_fec_packet = [] #list after adding xor of packets
     seq = 0 #sequence number
 
@@ -34,19 +33,10 @@ def main():
             #print sys.getsizeof(pkt)
             seq += 1
 
-        #user enters % loss in packet transmission
-        percent = sys.argv[1] if len(sys.argv) > 1 else '0'
-        if percent == "0":
-            print "Injecting 0 percent loss in the transmission."
-        elif percent > "0":
-            print "Injecting {} percent loss in the transmission".format(percent)
-        packets_tolose = calcLoss(int(percent),(seq+1))
-        print "Number of packets to lose {}".format(packets_tolose)
-
         #dropping packets to inject loss
-        injected_packet = list(initial_packet)
-        for i in range(int(packets_tolose)):
-            del injected_packet[i]
+        # injected_packet = list(initial_packet)
+        # for i in range(int(packets_tolose)):
+        #     del injected_packet[i]
 
         try:
             #socket connections and operations
@@ -55,6 +45,13 @@ def main():
             print "Server started at {}, port {}.".format(host,port)
             print '-------------------------------------------'
 
+            #user enters % loss in packet transmission
+            percent = sys.argv[1] if len(sys.argv) > 1 else '0'
+            if percent == "0":
+                print "Injecting 0 percent loss in the transmission."
+            elif percent > "0":
+                print "Injecting {} percent loss in the transmission".format(percent)
+
             #FEC selector: can be 'triple' or 'xor'. Default is xor
             selection = sys.argv[2] if len(sys.argv) == 3 else 'xor'
 
@@ -62,11 +59,19 @@ def main():
             if selection == "triple":
                 print "Proceeding with triple redundancy FEC"
                 print '-------------------------------------------'
-                for pkt in injected_packet:
+                packets_tolose = calcLoss(int(percent),((seq+1)*3))
+                print "Number of packets to lose {}".format(packets_tolose)
+                x = 0
+                for pkt in initial_packet:
                      for i in range (3):
-                         s.sendto(pkt,(host,port))
-                         print "sent: Packet:{}" .format(pkt)
-                     time.sleep(2)
+                         if x < packets_tolose:
+                             x += 1
+                             continue
+                         else:
+                            s.sendto(pkt,(host,port))
+                            print "sent: Packet:{}" .format(pkt)
+                            # time.sleep(2)
+
                 print "All packets sent."
 
             #XOR FEC
@@ -75,24 +80,34 @@ def main():
                 print '-------------------------------------------'
 
                 #Calculating and preparing xor fec packet
+                number_of_packets = math.ceil((seq+1)+((seq+1)/2.0))
+                packets_tolose = calcLoss(int(percent),number_of_packets)
+                print "Number of packets to lose {}".format(packets_tolose)
                 xor_list = []
                 xor_fec_packet = list (initial_packet)
-                ind = 3
+                ind = 2
                 n = 0
                 for i in range(0, len(initial_packet), 2):
-                    xor_list.append(fec_xor(initial_packet[i],initial_packet[i+1]))
+                    pkt = "->"+fec_xor(initial_packet[i],initial_packet[i+1])+"#"+str(datetime.datetime.now())
+                    xor_list.append(pkt)
 
                 while ind < len(xor_fec_packet):
                     xor_fec_packet.insert(ind, xor_list[n])
-                    ind += 4
+                    ind += 3
                     n += 1
+                xor_fec_packet.append("->"+fec_xor(initial_packet[seq-2],initial_packet[seq-1])+"#"+str(datetime.datetime.now()))
 
-                print "\n\n xor fec packet"
-                print xor_fec_packet
+                x = 0
+                for pkt in xor_fec_packet:
+                     if x < packets_tolose:
+                         x += 1
+                         continue
+                     else:
+                         s.sendto(pkt,(host,port))
+                         print "sent: Packet:{}" .format(pkt)
+                         time.sleep(2)
 
-
-
-
+                print "All packets sent."
 
         except socket.error, e:
             print "Error creating socket {}".format(e)
